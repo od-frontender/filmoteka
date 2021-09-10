c196194a7ed6a37599d8151a12162ea67a497102
od-frontender <od.frontender@gmail.com>
2021-09-10 15:27:07 +0300

    добавил паршалы

--- src/js/intersectionObserver.js
+++ src/js/intersectionObserver.js
@@ -1,23 +1,21 @@
 import API from './apiService.js';
-import incrementPage from './apiService.js';
+// import incrementPage from './apiService.js';
 import refs from './refs';
 import showGallery from './app.js';
-import apiService from './app.js';
+// import apiService from './app.js';
+const apiService = new API();
 
-
-
-  const onEntry = entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting && apiService.query !== '') {
-        // console.log('Load more movies + Date.now());
-        showGallery();
-        incrementPage();
-      }
-    });
-  };
-  
-  const observer = new IntersectionObserver(onEntry, {
-    rootMargin: '400px',
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && apiService.query !== '') {
+      // console.log('Load more movies + Date.now());
+      showGallery();
+      apiService.incrementPage();
+    }
   });
-  observer.observe(refs.sentinel);
-  
+};
+
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '400px',
+});
+observer.observe(refs.sentinel);
