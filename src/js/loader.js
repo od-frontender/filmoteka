5ddc5f6555257511463567ea9c0811b58e57dac2
ITgrand <vitaliidovhopolyk@gmail.com>
2021-09-11 16:20:50 +0300

    +

---
+++ src/js/loader.js
@@ -0,0 +1,13 @@
+const wrapper = document.querySelector('.loader-wrapper');
+
+loadSpinner();
+
+function loadSpinner() {
+    window.addEventListener('load', () => {
+    wrapper.classList.add('.hide');
+    setTimeout(() => {
+        wrapper.remove();
+    }, 1000);
+})
+}
+
