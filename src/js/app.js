bcba59122cce9e864fe6e250e0c5ef9340cdc35e
annasabbagh <sabbaghzadeh.anna@gmail.com>
2021-09-11 02:00:31 +0200

    infinite scroll

--- src/js/app.js
+++ src/js/app.js
@@ -12,34 +12,43 @@ function parseGenres(array) {
     genre_ids: el.genre_ids.length
       ? [
           ...genres.reduce(
-            (acc, { id, name }) => (el.genre_ids.includes(+id) ? [...acc, name].slice(0, 3) : acc),
-            [],
+            (acc, { id, name }) => (
+              el.genre_ids.includes(+id) 
+              ? [...acc, name].slice(0, 3) 
+              : acc), [],
           ),
         ]
       : ['Unknown'],
   }));
 }
-// Тестовая функция, нужно доделать
-// Функция парсит дату и заголовок для карточки галлереи
+
+// Функция парсит заголовок, дату и жанры для карточки галлереи - Тестовая функция, нужно доделать
+
 // function parseMoviesObject(array) {
 //   array.forEach(elem => {
+//     if (elem.title.length > 35) {
+//       elem.title = elem.title.slice(0, 35) + '...';
+//     }
+   
 //     elem.release_date
 //       ? (elem.release_date = elem.release_date.slice(0, 4))
 //       : (elem.release_date = 'Unknown');
-//   });
 
-//   return array.map(el => ({
-//     ...el,
-//     genre_ids: el.genre_ids.length
-//       ? [
-//           ...genres.reduce(
-//             (acc, { id, name }) => (el.genre_ids.includes(+id) ? [...acc, name] : acc),
-//             [],
-//           ),
-//         ]
-//       : ['Unknown'],
-//   }));
+//     // Функция от ментора - парсит жанры для карточки галлереи
+//     array.map(el => ({
+//       ...el,
+//       genre_ids: el.genre_ids.length
+//           ? [...genres.reduce(
+//             (acc, { id, name }) => (el.genre_ids.includes(+id) 
+//                   ? [...acc, name].slice(0, 3) 
+//                   : acc), [],
+//               ),]
+//           : ['Unknown'],
+//       }));
+//   });
+//   return array;
 // }
+
 
 // Функция выводит список популярных фильмов на основную старницу
 export default function showGallery() {
