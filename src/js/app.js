cb5d11a172811f52c49be1199f5f483114fd4c30
ITgrand <vitaliidovhopolyk@gmail.com>
2021-09-12 23:12:36 +0300

    update foo genres

--- src/js/app.js
+++ src/js/app.js
@@ -6,54 +6,54 @@ import API from './apiService';
 const apiService = new API();
 
 // Функция парсит жанры для карточки галлереи
-function parseGenres(array) {
-  return array.map(el => ({
-    ...el,
-    genre_ids: el.genre_ids.length
-      ? [
-          ...genres.reduce(
-            (acc, { id, name }) => (
-              el.genre_ids.includes(+id) 
-              ? [...acc, name].slice(0, 3) 
-              : acc), [],
-          ),
-        ]
-      : ['Unknown'],
-  }));
-}
+// function parseGenres(array) {
+//   return array.map(el => ({
+//     ...el,
+//     genre_ids: el.genre_ids.length
+//       ? [
+//           ...genres.reduce(
+//             (acc, { id, name }) => (
+//               el.genre_ids.includes(+id) 
+//               ? [...acc, name].slice(0, 3) 
+//               : acc), [],
+//           ),
+//         ]
+//       : ['Unknown'],
+//   }));
+// }
 
 // Функция парсит заголовок, дату и жанры для карточки галлереи - Тестовая функция, нужно доделать
 
-// function parseMoviesObject(array) {
-//   array.forEach(elem => {
-//     if (elem.title.length > 35) {
-//       elem.title = elem.title.slice(0, 35) + '...';
-//     }
+function parseMoviesObject(array) {
+  array.forEach(elem => {
+    if (elem.title.length > 35) {
+      elem.title = elem.title.slice(0, 35) + '...';
+    }
    
-//     elem.release_date
-//       ? (elem.release_date = elem.release_date.slice(0, 4))
-//       : (elem.release_date = 'Unknown');
+    elem.release_date
+      ? (elem.release_date = elem.release_date.slice(0, 4))
+      : (elem.release_date = 'Unknown');
 
-//     // Функция от ментора - парсит жанры для карточки галлереи
-//     array.map(el => ({
-//       ...el,
-//       genre_ids: el.genre_ids.length
-//           ? [...genres.reduce(
-//             (acc, { id, name }) => (el.genre_ids.includes(+id) 
-//                   ? [...acc, name].slice(0, 3) 
-//                   : acc), [],
-//               ),]
-//           : ['Unknown'],
-//       }));
-//   });
-//   return array;
-// }
+    // Функция от ментора - парсит жанры для карточки галлереи
+    
+  });
+  return array.map(el => ({
+      ...el,
+      genre_ids: el.genre_ids.length
+          ? [...genres.reduce(
+            (acc, { id, name }) => (el.genre_ids.includes(+id) 
+                  ? [...acc, name].slice(0, 2) 
+                  : acc), [],
+              ),]
+          : ['Unknown'],
+      }));;
+}
 
 
 // Функция выводит список популярных фильмов на основную старницу
 export default function showGallery() {
   apiService.fetchPopularFilms().then(response => {
-    const parsedData = parseGenres(response.results);
+    const parsedData = parseMoviesObject(response.results);
     renderMoviesList(parsedData);
   });
 }
